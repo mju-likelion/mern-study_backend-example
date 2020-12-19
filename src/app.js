1a42c53ba7c35ff7a5e1dac62bd9255840c1a12d
soultree-fly <yuyaebean@gmail.com>
2020-12-19 12:47:18 +0900

    1.0 Connect to MongoDB using mongoose w/ dotenv

--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
+/* eslint-disable no-console */
 import Koa from 'koa';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const { PORT, MONGO_URI } = process.env;
+
+if (!MONGO_URI) {
+  throw Error('mongodb uri가 존재하지 않습니다.');
+}
+
+async function connectDB() {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (e) {
+    console.error(e);
+  }
+}
+connectDB();
 
 const app = new Koa();
 
@@ -6,6 +31,7 @@ app.use(ctx => {
   ctx.body = 'hello world';
 });
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 });
